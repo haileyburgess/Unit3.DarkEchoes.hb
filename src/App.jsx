f03759c3caab5ccfabad7879ac6bd37b76102b03
Hailey Burgess <hailey.burgess@redcross.org>
2025-04-28 21:57:04 -0400

    basic functionality working

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,53 @@
+import { useState } from "react";
+import { episodeList } from "./data"; //get static data from json file
+
 export default function App() {
   // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Dark Echoes</h2>
+          <p>Select an Episode for Details:</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="details">
+        <h2>{selectedEpisode.title}</h2>
+        <p>{selectedEpisode.description}</p>
+      </section>
+    );
+  }
+
+  function Season() {
+    return (
+    <section className="season">
+      <h2>Season 1</h2>
+      <ul className="season">
+        {episodes.map((episode) => (
+          <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+            {episode.title}
+          </li>
+        ))}
+      </ul>
+    </section>
+    );
+  }
+
+  return (
+    <>
+    <header>
+      <h1>Dark Echoes</h1>
+    </header>
+    <main>
+      <Season />
+      <EpisodeDetails />
+    </main>
+    </>
+  );
 }
